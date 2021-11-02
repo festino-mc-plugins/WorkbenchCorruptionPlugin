f8bda348461cf861ce64b748e41539ac4038a64b
Festino <offestashka@mail.ru>
2021-11-02 19:57:53 +0300

    Concurrency preparing

--- src/com/festp/RandomTicker.java
+++ src/com/festp/RandomTicker.java
@@ -3,6 +3,7 @@ package com.festp;
 import java.util.Random;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
@@ -11,6 +12,7 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_17_R1.CraftWorld; // TODO use reflection
 import org.bukkit.craftbukkit.v1_17_R1.util.CraftMagicNumbers;
+import org.bukkit.util.Vector;
 
 import net.minecraft.core.BlockPosition;
 import net.minecraft.world.level.block.state.IBlockData;
@@ -44,137 +46,166 @@ public class RandomTicker {
 	{
 		for (World w : Bukkit.getWorlds())
 		{
-			int minYblock = w.getMinHeight();
-			int maxYblock = w.getMaxHeight();
-			int minY = minYblock / 16;
-			int maxY = maxYblock / 16;
-			int sectionHeight = maxY - minY;
-			int times = chunkTicks / (16 * 16 * sectionHeight * 16);
-
 			Set<Long> loadedChunks = new HashSet<>();
 			for (Chunk c : w.getLoadedChunks())
 				loadedChunks.add(((long) c.getZ()) << 32 + c.getX());
-			
+
+			ConcurrentLinkedQueue<Vector3i> blocksToGrow = new ConcurrentLinkedQueue<>();
+			ConcurrentLinkedQueue<Block> blocksToSet = new ConcurrentLinkedQueue<>();
 			net.minecraft.world.level.World nmsWorld = ((CraftWorld) w).getHandle();
 			for (Chunk c : w.getLoadedChunks())
 			{
-			    net.minecraft.world.level.chunk.Chunk nmsChunk = nmsWorld.getChunkAt(c.getX(), c.getZ());
-			    net.minecraft.world.level.chunk.ChunkSection[] chunksections = nmsChunk.getSections();
-				for (int sectionIndex = 0; sectionIndex < sectionHeight; sectionIndex++)
+				tickChunkRandomly(c, nmsWorld, blocksToGrow, blocksToSet); // TODO parallel
+			}
+			/*for (Chunk c : w.getLoadedChunks())
+			{
+				tickChunkRegularly(c, nmsWorld);
+			}*/
+
+			while (!blocksToGrow.isEmpty())
+			{
+				Vector3i to = blocksToGrow.remove();
+				if (loadedChunks.contains(((long) to.z >> 4) << 32 + to.x >> 4))
 				{
-					net.minecraft.world.level.chunk.ChunkSection section = chunksections[sectionIndex];
-					if (section == null) // || !section.shouldTick()
-						continue;
-					
-					for (int i = 0; i < randomSectionTicks; i++)
-					{
-						rSeed = rSeed * 3 + 1013904223; // https://github.com/Bukkit/mc-dev/blob/c1627dc9cc7505581993eb0fa15597cb36e94244/net/minecraft/server/WorldServer.java#L214
-						int xyz = rSeed >> 2;
-						int x = xyz & 0x0F;
-						xyz >>= 8;
-						int z = xyz & 0x0F;
-						xyz >>= 8;
-						int y = xyz & 0x0F;
-						xyz >>= 8;
-						
-						IBlockData ibd = section.getType(x, y, z);
-						
-						if (ibd == WORKBENCH_DATA)
-						{
-							y += section.getYPosition();
-							int dir = random.nextInt(6);
-							
-							if (dir == 0)
-								x--;
-							else if (dir == 1)
-								x++;
-							else if (dir == 2)
-								y--;
-							else if (dir == 4)
-								z--;
-							else if (dir == 5)
-								z++;
-							else //if (dir == 3)
-								y++;
-							
-							if (y < minYblock || y >= maxYblock)
-								continue;
-
-							int dx = x >> 4;
-							int dz = z >> 4;
-						    net.minecraft.world.level.chunk.Chunk nmsChunkTo;
-							if (dx == 0 && dz == 0) {
-								nmsChunkTo = nmsChunk;
-							} else {
-								Long chunk = ((long) c.getZ() + dz) << 32 + c.getX() + dx;
-								if (loadedChunks.contains(chunk)) {
-									nmsChunkTo = nmsWorld.getChunkAt(c.getX() + dx, c.getZ() + dz);
-									x -= dx << 4;
-									z -= dz << 4;
-								} else {
-									continue;
-								}
-							}
-
-							BlockPosition bp = new BlockPosition(x, y, z);
-							ibd = nmsChunkTo.getType(bp);
-							
-							if (!ibd.isAir() && ibd != WORKBENCH_DATA && ibd != BEDROCK_DATA) {
-								//System.out.println(2 + " " + ibd + " " + (net.minecraft.world.level.block.Block.getCombinedId(ibd) >> 12));
-								Block to = w.getChunkAt(c.getX() + dx, c.getZ() + dz).getBlock(x, y, z);
-								to.setType(Material.CRAFTING_TABLE);
-							}
-						}
-					}
-				}
-				
-				for (int i = 0; i < regularChunkTicks; i++)
-				{
-					int hor = (chunkTicks & 0x00FF);
-					int direction = (chunkTicks & 0x0100) >> 8;
-					int b1 = (chunkTicks & 0x0E00) >> 9;
-					int section = (chunkTicks & 0xFFFFF000) >> 12;
-					section = (section * 5 + b1 + direction + times % 11) % 16;
-					section = minY + section % sectionHeight;
-					int y = (b1 + section * 8) * 2 + direction;
-					if (direction == 1)
-				        y = 256 - y;
-					int xz = (hor * 55 + y * y + times % 17) % 256;
-					int x = xz % 16;
-					int z = xz / 16;
-					Block b = c.getBlock(x, y, z);
-					if (b.getType() == Material.CRAFTING_TABLE)
-					{
-						int dir = random.nextInt() % 6;
-						if (dir < 0)
-							dir += 6;
-						
-						Block to = null;
-						if (dir == 0)
-							to = b.getRelative(0, 0, -1);
-						else if (dir == 1)
-							to = b.getRelative(0, 0, 1);
-						else if (dir == 2)
-							to = b.getRelative(0, -1, 0);
-						else if (dir == 3)
-							to = b.getRelative(0, 1, 0);
-						else if (dir == 4)
-							to = b.getRelative(-1, 0, 0);
-						else if (dir == 5)
-							to = b.getRelative(1, 0, 0);
-						
-						// if "to" is not loaded
-
-						Material toMaterial = to.getType();
-						if (!toMaterial.isAir() && toMaterial != Material.CRAFTING_TABLE && toMaterial != Material.BEDROCK)
-							to.setType(Material.CRAFTING_TABLE);
-					}
-					chunkTicks++;
+					Block toBlock = w.getBlockAt(to.x, to.y, to.z);
+					Material toMaterial = toBlock.getType();
+					if (!toMaterial.isAir() && toMaterial != Material.CRAFTING_TABLE && toMaterial != Material.BEDROCK)
+						blocksToSet.add(toBlock);
 				}
 			}
 			
-			//System.out.println(w.getLoadedChunks().length + " " + zMap.size());
+			while (!blocksToSet.isEmpty())
+			{
+				Block to = blocksToSet.remove();
+				to.setType(Material.CRAFTING_TABLE);
+			}
 			loadedChunks.clear();
+		}
+		chunkTicks++;
+	}
+	
+	private void tickChunkRandomly(Chunk c, net.minecraft.world.level.World nmsWorld,
+			ConcurrentLinkedQueue<Vector3i> blocksToGrow, ConcurrentLinkedQueue<Block> blocksToSet)
+	{
+	    net.minecraft.world.level.chunk.Chunk nmsChunk = nmsWorld.getChunkAt(c.getX(), c.getZ());
+	    net.minecraft.world.level.chunk.ChunkSection[] chunksections = nmsChunk.getSections();
+		for (int sectionIndex = 0; sectionIndex < nmsWorld.getSectionsCount(); sectionIndex++)
+		{
+			net.minecraft.world.level.chunk.ChunkSection section = chunksections[sectionIndex];
+			if (section == null) // || !section.shouldTick()
+				continue;
+			
+			for (int i = 0; i < randomSectionTicks; i++)
+			{
+				rSeed = rSeed * 3 + 1013904223; // https://github.com/Bukkit/mc-dev/blob/c1627dc9cc7505581993eb0fa15597cb36e94244/net/minecraft/server/WorldServer.java#L214
+				int xyz = rSeed >> 2;
+				int x = xyz & 0x0F;
+				xyz >>= 8;
+				int z = xyz & 0x0F;
+				xyz >>= 8;
+				int y = xyz & 0x0F;
+				xyz >>= 8;
+				
+				IBlockData ibd = section.getType(x, y, z);
+				
+				if (ibd == WORKBENCH_DATA)
+				{
+					y += section.getYPosition();
+					int dir = random.nextInt(6);
+					
+					if (dir == 0)
+						x--;
+					else if (dir == 1)
+						x++;
+					else if (dir == 2)
+						y--;
+					else if (dir == 4)
+						z--;
+					else if (dir == 5)
+						z++;
+					else //if (dir == 3)
+						y++;
+					
+					if (y < nmsWorld.getMinBuildHeight() || y >= nmsWorld.getMaxBuildHeight())
+						continue;
+
+					int dx = x >> 4;
+					int dz = z >> 4;
+				    net.minecraft.world.level.chunk.Chunk nmsChunkTo;
+					if (dx == 0 && dz == 0) {
+						nmsChunkTo = nmsChunk;
+					} else {
+						/*Long chunk = ((long) c.getZ() + dz) << 32 + c.getX() + dx;
+						if (loadedChunks.contains(chunk)) {
+							nmsChunkTo = nmsWorld.getChunkAt(c.getX() + dx, c.getZ() + dz);
+							x -= dx << 4;
+							z -= dz << 4;
+						} else {
+							continue;
+						}*/
+						x -= dx << 4;
+						z -= dz << 4;
+						blocksToGrow.add(new Vector3i(x , y, z));
+						continue;
+					}
+
+					BlockPosition bp = new BlockPosition(x, y, z);
+					ibd = nmsChunkTo.getType(bp);
+					
+					if (!ibd.isAir() && ibd != WORKBENCH_DATA && ibd != BEDROCK_DATA) {
+						//System.out.println(2 + " " + ibd + " " + (net.minecraft.world.level.block.Block.getCombinedId(ibd) >> 12));
+						/*Block to = w.getChunkAt(c.getX() + dx, c.getZ() + dz).getBlock(x, y, z);*/
+						blocksToSet.add(c.getBlock(x, y, z));
+					}
+				}
+			}
+		}
+	}
+
+	private void tickChunkRegularly(Chunk c, net.minecraft.world.level.World nmsWorld)
+	{
+		int times = chunkTicks / (16 * 16 * nmsWorld.getSectionsCount() * 16);
+		for (int i = 0; i < regularChunkTicks; i++)
+		{
+			int hor = (chunkTicks & 0x00FF);
+			int direction = (chunkTicks & 0x0100) >> 8;
+			int b1 = (chunkTicks & 0x0E00) >> 9;
+			int section = (chunkTicks & 0xFFFFF000) >> 12;
+			section = (section * 5 + b1 + direction + times % 11) % 16;
+			section = nmsWorld.getMinSection() + section % nmsWorld.getSectionsCount();
+			int y = (b1 + section * 8) * 2 + direction;
+			if (direction == 1)
+		        y = 256 - y;
+			int xz = (hor * 55 + y * y + times % 17) % 256;
+			int x = xz % 16;
+			int z = xz / 16;
+			Block b = c.getBlock(x, y, z);
+			if (b.getType() == Material.CRAFTING_TABLE)
+			{
+				int dir = random.nextInt() % 6;
+				if (dir < 0)
+					dir += 6;
+				
+				Block to = null;
+				if (dir == 0)
+					to = b.getRelative(0, 0, -1);
+				else if (dir == 1)
+					to = b.getRelative(0, 0, 1);
+				else if (dir == 2)
+					to = b.getRelative(0, -1, 0);
+				else if (dir == 3)
+					to = b.getRelative(0, 1, 0);
+				else if (dir == 4)
+					to = b.getRelative(-1, 0, 0);
+				else if (dir == 5)
+					to = b.getRelative(1, 0, 0);
+				
+				// if "to" is not loaded
+
+				Material toMaterial = to.getType();
+				if (!toMaterial.isAir() && toMaterial != Material.CRAFTING_TABLE && toMaterial != Material.BEDROCK)
+					to.setType(Material.CRAFTING_TABLE);
+			}
 		}
 	}
 }
---
+++ src/com/festp/Vector3i.java
@@ -0,0 +1,12 @@
+package com.festp;
+
+public class Vector3i {
+	public final int x, y, z;
+	
+	public Vector3i(int x, int y, int z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
